86670a5bf5c0c0dd35b2092a2dea1113c4cf75a9
artreysan <arthur.rs0313@gmail.com>
2024-06-09 20:38:37 -0600

    feat: finish first endpoint

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -3,7 +3,6 @@ import {connect} from '../database.js'
 export const getTasks = async (req, res) => {
     const db = await connect();
     const [rows]= await db.query('SELECT * FROM tasks');
-    console.log(rows);
     res.json(rows);
 }
 
@@ -16,24 +15,37 @@ export const getTask = async (req, res) => {
 
 export const getTaskCount = async (req, res) => {
     const db = await connect();
-    const response = await db.query('SELECT COUNT(*) FROM tasks');
-    res.json(response[0][0]["COUNT(*)"]);
+    const [response] = await db.query('SELECT COUNT(*) FROM tasks');
+    res.json(response[0]["COUNT(*)"]);
 }
 
 export const saveTask = async (req, res) => {
     const db = await connect();
-    const response = await db.query('INSERT INTO tasks (title, description_task) VALUES (?,?)', [req.body.title, req.body.description]);
-    res.json(response);
+    const [response ] = await db.query('INSERT INTO tasks (title, description_task) VALUES (?,?)', [
+        req.body.title, 
+        req.body.description_task]);
+        console.log(response);
+    res.json({
+        id: response.insertId,
+        ... req.body
+
+    });
 }
 
 export const deleteTask = async (req, res) => {
     const db = await connect();
-    const response = await db.query('SELECT COUNT(*) FROM tasks');
-    res.json(response);
+    await db.query('DELETE FROM tasks WHERE id = ? ',[
+        req.params.id
+    ]);
+    res.sendStatus(204);
 }
 
 export const updateTask = async (req, res) => {
     const db = await connect();
-    const response = await db.query('SELECT COUNT(*) FROM tasks');
+    const response = await db.query('UPDATE tasks SET ? WHERE id = ? ',[
+        req.body,
+        req.params.id
+    ]);
+
     res.json(response);
 }
